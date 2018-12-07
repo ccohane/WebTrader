0b952a0376e845a26eec8af7eaec4dc20dc2dcbf
ccohane <ccohane@gmail.com>
2018-12-07 14:00:16 -0500

    Added a wsqi.py to let omnibus run the application

---
+++ run/wsgi.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+from src import omnibus
+
+if __name__ == '__main__':
+    # FIXME Change the following parameters before deploying this application.
+    omnibus.run(host='0.0.0.0',port=1996,debug=True)
